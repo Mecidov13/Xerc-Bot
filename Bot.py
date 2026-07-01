9d3fc0acd5ae36e123f1755cff82a06736918b2c
Mecidov13 <royalmecidov148@gmail.com>
2026-07-02 02:44:30 +0400

    Add default bot properties and parse mode

--- Bot.py
+++ Bot.py
@@ -3,6 +3,8 @@ import logging
 import os
 
 from aiogram import Bot, Dispatcher, F
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
@@ -33,7 +35,7 @@ CATEGORIES = [
     "📦 Digər",
 ]
 
-bot = Bot(token=BOT_TOKEN)
+bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher(storage=MemoryStorage())
 
 
